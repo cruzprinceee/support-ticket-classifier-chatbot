287e6e9a4effd5d5ce708c64fa5af2809dfaf5c8
lubba-lubba-dub-dub <148734329+Lansuuuu@users.noreply.github.com>
2024-11-29 09:02:40 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from models.classifier import classify_ticket
 from utils.helpers import chatbot_response
 
-st.title("Automated Support Ticket Chatbot")
+st.title("Support Bot")
 
 user_query = st.text_input("Enter your support ticket query:")
 if user_query:
